6ab97f01a05bfd8132725d2ed3351f3f20460d91
winniepopu <winniepopu@gmail.com>
2019-04-12 09:56:24 +0800

    section2 first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// introAppear();
+
+// function introAppear() {
+//     $(".intro-left").animate({
+//         'margin-left': '0px'
+//     }, 2000)
+// }
+
+$(".intro-left").animate({
+    'margin-left': '0px'
+}, 2000)
