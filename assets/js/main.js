98945cb25f647fb2bca4ee93f9489078feb010cb
XiaoTingYi <a75896453@gmail.com>
2019-04-14 23:29:10 +0800

    section3190414

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,40 @@
 
 $(".intro-left").animate({
     'margin-left': '0px'
-}, 2000)
+}, 2000);
+
+var sourceSwap = function () {
+    var $this = $(this);
+    var newSource = $this.data('alt-src');
+    $this.data('alt-src', $this.attr('src'));
+    $this.attr('src', newSource);
+}
+
+var S3=false;
+
+$(function () {
+    $('.S3line').hide();
+    $('.introTeam').hide();
+    $('.personalImg').hover(sourceSwap, sourceSwap);
+    $(window).scroll(function () {
+        var scrollVal = $(this).scrollTop();
+        if(scrollVal > 500 && S3==false){
+            $('.S3line').delay(1000).transition('fade',1000);
+            //setTimeout(introT,500);
+            $('.introTeam').delay(5000).transition('fade',500);
+            /*$('.S3line').transition('fade', 500);
+            $('.introTeam').transition('fade', 500);*/
+            S3=true;
+          }
+        else if(scrollVal <300 && S3==true){
+            S3=false;
+            $('.S3line').transition('fade',1000);
+            $('.introTeam').transition('fade',500);
+          }
+      });
+});
+
+function introT(){
+    $('.introTeam').transition('fade',500);
+}
+
